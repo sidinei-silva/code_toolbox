321aa2beb32625ce4158468177e4fa7c8e277d72
Sidinei Silva <sidinei.silva02@gmail.com>
2020-03-02 17:09:04 -0300

    Include configs eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,26 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
   },
 };
