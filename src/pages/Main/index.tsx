eb5612db59ec8fc4ffb7d15e83bd7bd87aec61e2
Sidinei Silva <sidinei.silva02@gmail.com>
2020-03-08 10:26:59 -0300

    Styled page Home

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Title, Content, Container } from './styles';
 
-// import { Container } from './styles';
+import Header from '../../components/Header';
+import Generator from '../../components/Generator';
 
 export default function Main() {
-  console.log('Main');
-  return <h1>Hello main</h1>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Title>Generators</Title>
+        <Content>
+          <Link to="/timestamp">
+            <Generator
+              title="TimeStamp"
+              description="Timestamp generator for javascript"
+            />
+          </Link>
+
+          <Generator
+            title="Box Shadow"
+            description="Timestamp generator for javascript"
+          />
+
+          <Generator title="Coming Soon" description="Coming Soon" />
+          <Generator title="Coming Soon" description="Coming Soon" />
+        </Content>
+      </Container>
+    </>
+  );
 }
