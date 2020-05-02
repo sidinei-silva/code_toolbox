7dfd91aab46458e002bc37805e0cef5364c75613
Sidinei Silva <sidinei.silva02@gmail.com>
2020-05-02 13:08:19 -0300

    Include Provider Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global';
+
+import theme from './theme';
+
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyle />
+        <Routes />
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
 }
