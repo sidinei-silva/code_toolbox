ad46b160ee87daffcda23d5ae54bc5307257f889
Sidinei Silva <sidinei.silva02@gmail.com>
2020-05-27 18:24:19 -0300

    Override document Next to usage SSR styled-components and material ui

---
+++ src/pages/_document.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import NextDocument from 'next/document';
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const materialUiSheets = new MaterialUiServerStyleSheets();
+    const styledComponentSheet = new StyledComponentSheets();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            ),
+        });
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      };
+    } finally {
+      styledComponentSheet.seal();
+    }
+  }
+}
