6b6b327f4a6889e4f75748082ce63d6ddbb1fece
Sidinei Silva <sidinei.silva02@gmail.com>
2020-05-28 14:52:25 -0300

    Usage react ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,29 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import NextApp from 'next/app';
+import Router from 'next/router';
 import { ThemeProvider } from 'styled-components';
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
+
+import { initGA, logPageView } from '../utils/analytics.js';
 
 import GlobalStyle from '../styles/global';
 
 import theme from '../theme';
 
 export default class App extends NextApp {
+  // Check if in production
+  isProduction = process.env.NODE_ENV === 'production';
+
+  componentDidMount() {
+    if (this.isProduction) {
+      initGA();
+      logPageView();
+      Router.events.on('routeChangeComplete', logPageView);
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
