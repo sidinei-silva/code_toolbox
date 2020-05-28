e1e1845576974a065cddead44ff3f44e0b3ccbe6
Sidinei Silva <sidinei.silva02@gmail.com>
2020-05-28 12:20:17 -0300

    Include links in page main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Link from 'next/link';
 import { Content, StyledContainer } from './styles';
 
 import Header from '../components/Header';
@@ -11,20 +13,33 @@ export default function Main() {
       <Header />
       <StyledContainer>
         <Content>
-          <Generator
-            title="TimeStamp"
-            description="Timestamp generator for javascript"
-          />
+          <Link href="/timestamp">
+            <a>
+              <Generator
+                title="TimeStamp"
+                description="Timestamp generator for javascript"
+              />
+            </a>
+          </Link>
 
-          <Generator
-            title="Border Radius"
-            description="Border Radius generator for CSS"
-          />
+          <Link href="/border-radius">
+            <a>
+              <Generator
+                title="Border Radius"
+                description="Border Radius generator for CSS"
+              />
+            </a>
+          </Link>
 
-          <Generator
-            title="Box Shadow"
-            description="Box Shadow generator for CSS"
-          />
+          <Link href="/box-shadow">
+            <a>
+              <Generator
+                title="Box Shadow"
+                description="Box Shadow generator for CSS"
+              />
+            </a>
+          </Link>
+
           <Generator title="Coming Soon" description="Coming Soon" />
         </Content>
       </StyledContainer>
