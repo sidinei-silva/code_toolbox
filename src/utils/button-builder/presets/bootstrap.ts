afb62d059362abc08bace2fc049976e72500bb28
Sidinei Silva <sidinei.silva02@gmail.com>
2020-06-15 20:16:19 -0300

    Include presets bootstrap buttons

---
+++ src/utils/button-builder/presets/bootstrap.ts
@@ -0,0 +1,596 @@
+export default {
+  btnPrimary: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 0, g: 123, b: 255, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 0, g: 123, b: 255, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 0, g: 105, b: 217, a: 1 },
+    hoverBorderColor: { r: 0, g: 98, b: 204, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 0, g: 98, b: 204, a: 1 },
+    activeBorderColor: { r: 0, g: 92, b: 191, a: 1 },
+  },
+
+  btnSecondary: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 108, g: 117, b: 125, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 108, g: 117, b: 125, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 90, g: 98, b: 104, a: 1 },
+    hoverBorderColor: { r: 84, g: 91, b: 98, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 84, g: 91, b: 98, a: 1 },
+    activeBorderColor: { r: 78, g: 85, b: 91, a: 1 },
+  },
+
+  btnSuccess: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 40, g: 167, b: 69, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 40, g: 167, b: 69, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 33, g: 136, b: 56, a: 1 },
+    hoverBorderColor: { r: 30, g: 126, b: 52, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 30, g: 126, b: 52, a: 1 },
+    activeBorderColor: { r: 28, g: 116, b: 48, a: 1 },
+  },
+
+  btnWarning: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 33, g: 37, b: 41, a: 1 },
+    boxColor: { r: 255, g: 193, b: 7, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 255, g: 193, b: 7, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    hoverBoxColor: { r: 224, g: 168, b: 0, a: 1 },
+    hoverBorderColor: { r: 211, g: 158, b: 0, a: 1 },
+
+    // active
+    activeFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    activeBoxColor: { r: 211, g: 158, b: 0, a: 1 },
+    activeBorderColor: { r: 198, g: 149, b: 0, a: 1 },
+  },
+
+  btnDanger: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 220, g: 53, b: 69, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 220, g: 53, b: 69, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 200, g: 35, b: 51, a: 1 },
+    hoverBorderColor: { r: 189, g: 33, b: 48, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 189, g: 33, b: 48, a: 1 },
+    activeBorderColor: { r: 178, g: 31, b: 45, a: 1 },
+  },
+
+  btnLight: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 33, g: 37, b: 41, a: 1 },
+    boxColor: { r: 248, g: 249, b: 250, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 248, g: 249, b: 250, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    hoverBoxColor: { r: 226, g: 230, b: 234, a: 1 },
+    hoverBorderColor: { r: 218, g: 224, b: 229, a: 1 },
+
+    // active
+    activeFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    activeBoxColor: { r: 218, g: 224, b: 229, a: 1 },
+    activeBorderColor: { r: 211, g: 217, b: 223, a: 1 },
+  },
+
+  btnLink: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 0, g: 123, b: 255, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 255, g: 255, b: 255, a: 0 },
+
+    // Hover
+    hoverFontColor: { r: 0, g: 86, b: 179, a: 1 },
+    hoverBoxColor: { r: 255, g: 255, b: 255, a: 0 },
+    hoverBorderColor: { r: 255, g: 255, b: 255, a: 0 },
+
+    // active
+    activeFontColor: { r: 0, g: 123, b: 255, a: 1 },
+    activeBoxColor: { r: 255, g: 255, b: 255, a: 0 },
+    activeBorderColor: { r: 255, g: 255, b: 255, a: 0 },
+  },
+
+  btnInfo: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 23, g: 162, b: 184, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 23, g: 162, b: 184, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 19, g: 132, b: 150, a: 1 },
+    hoverBorderColor: { r: 17, g: 122, b: 139, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 19, g: 132, b: 150, a: 1 },
+    activeBorderColor: { r: 17, g: 122, b: 139, a: 1 },
+  },
+
+  btnDark: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 255, b: 255, a: 1 },
+    boxColor: { r: 52, g: 58, b: 64, a: 1 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 52, g: 58, b: 64, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 35, g: 39, b: 43, a: 1 },
+    hoverBorderColor: { r: 29, g: 33, b: 36, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 35, g: 39, b: 43, a: 1 },
+    activeBorderColor: { r: 29, g: 33, b: 36, a: 1 },
+  },
+
+  btnOutlinePrimary: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 0, g: 123, b: 255, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 0, g: 123, b: 255, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 0, g: 123, b: 255, a: 1 },
+    hoverBorderColor: { r: 0, g: 123, b: 255, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 0, g: 123, b: 255, a: 1 },
+    activeBorderColor: { r: 0, g: 123, b: 255, a: 1 },
+  },
+
+  btnOutlineSecondary: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 108, g: 117, b: 125, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 108, g: 117, b: 125, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 108, g: 117, b: 125, a: 1 },
+    hoverBorderColor: { r: 108, g: 117, b: 125, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 108, g: 117, b: 125, a: 1 },
+    activeBorderColor: { r: 108, g: 117, b: 125, a: 1 },
+  },
+
+  btnOutlineSuccess: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 40, g: 167, b: 69, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 40, g: 167, b: 69, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 40, g: 167, b: 69, a: 1 },
+    hoverBorderColor: { r: 40, g: 167, b: 69, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 30, g: 126, b: 52, a: 1 },
+    activeBorderColor: { r: 28, g: 116, b: 48, a: 1 },
+  },
+
+  btnOutlineWarning: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 255, g: 193, b: 7, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 255, g: 193, b: 7, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    hoverBoxColor: { r: 255, g: 193, b: 7, a: 1 },
+    hoverBorderColor: { r: 255, g: 193, b: 7, a: 1 },
+
+    // active
+    activeFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    activeBoxColor: { r: 255, g: 193, b: 7, a: 1 },
+    activeBorderColor: { r: 255, g: 193, b: 7, a: 1 },
+  },
+
+  btnOutlineDanger: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 220, g: 53, b: 69, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 220, g: 53, b: 69, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 220, g: 53, b: 69, a: 1 },
+    hoverBorderColor: { r: 220, g: 53, b: 69, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 220, g: 53, b: 69, a: 1 },
+    activeBorderColor: { r: 220, g: 53, b: 69, a: 1 },
+  },
+
+  btnOutlineLight: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 248, g: 249, b: 250, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 248, g: 249, b: 250, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    hoverBoxColor: { r: 248, g: 249, b: 250, a: 1 },
+    hoverBorderColor: { r: 248, g: 249, b: 250, a: 1 },
+
+    // active
+    activeFontColor: { r: 33, g: 37, b: 41, a: 1 },
+    activeBoxColor: { r: 248, g: 249, b: 250, a: 1 },
+    activeBorderColor: { r: 248, g: 249, b: 250, a: 1 },
+  },
+
+  btnOutlineInfo: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 23, g: 162, b: 184, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 23, g: 162, b: 184, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 23, g: 162, b: 184, a: 1 },
+    hoverBorderColor: { r: 23, g: 162, b: 184, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 23, g: 162, b: 184, a: 1 },
+    activeBorderColor: { r: 23, g: 162, b: 184, a: 1 },
+  },
+
+  btnOutlineDark: {
+    // Size
+    horizontalPadding: 16,
+    verticalPadding: 8,
+    pointerCheck: true,
+
+    fontColor: { r: 52, g: 58, b: 64, a: 1 },
+    boxColor: { r: 255, g: 255, b: 255, a: 0 },
+    fontWeight: '400',
+    fontSize: 16,
+
+    // shadow
+    shadowHorizontalLength: 0,
+    shadowVerticalLength: 0,
+    shadowBlurRadius: 0,
+    shadowSpreadRadius: 0,
+    shadowColor: { r: 0, g: 0, b: 0, a: 0 },
+
+    // Border Radius
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: { r: 52, g: 58, b: 64, a: 1 },
+
+    // Hover
+    hoverFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    hoverBoxColor: { r: 52, g: 58, b: 64, a: 1 },
+    hoverBorderColor: { r: 52, g: 58, b: 64, a: 1 },
+
+    // active
+    activeFontColor: { r: 255, g: 255, b: 255, a: 1 },
+    activeBoxColor: { r: 52, g: 58, b: 64, a: 1 },
+    activeBorderColor: { r: 52, g: 58, b: 64, a: 1 },
+  },
+};
